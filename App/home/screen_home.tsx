6b905cfe1c801b003fb38bd0b13948f16dfaa053
michelonwordi <michel.onwordi@gmail.com>
2021-09-09 21:20:14 +0200

    Add logo

--- App/home/screen_home.tsx
+++ App/home/screen_home.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import {StatusBar, StyleSheet, View} from 'react-native';
+import {Dimensions, Image, StatusBar, StyleSheet, View} from 'react-native';
 import colors from '../colors';
+
+const windowSize = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.blue,
     flex: 1,
+    justifyContent: 'center'
+  },
+  logoContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    position: 'absolute',
+    height: '25%',
+    width: '25%',
+  },
+  logoBackground: {
+    height:'45%',
+    width: '45%',
   },
 });
 
 export const Home = () => {
   return (
     <View style={styles.container}>
-      <StatusBar barStyle='light-content' backgroundColor={colors.blue} />
+      <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../assets/images/background.png')}
+          style={styles.logoBackground}
+          resizeMode="contain"
+        />
+        <Image
+          source={require('../assets/images/logo.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
     </View>
   );
 };
